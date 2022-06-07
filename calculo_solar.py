4763db61405913c902a4dd2fe62ecf530472614e
Ricardo S. Lopes <68877071+rickslopes@users.noreply.github.com>
2022-06-07 00:05:46 -0300

    Add files via upload

---
+++ calculo_solar.py
@@ -0,0 +1,59 @@
+#BY RICARDO SOUZA LOPES - rickslopes@gmail.com
+#LICENÇA MIT
+
+import math
+
+class CalculoUsinaSolar:
+    def __init__(self, potencia_placas):
+        self._potencia_placas = potencia_placas
+        self.kwp = 0
+        self.quantidade_placas = 0
+        self.kwp_entregue = 0
+
+    @property
+    def potencia_placas(self):
+        return self._potencia_placas
+
+    def tranforma_em_kwp(self):
+        quilowatt_dia = self.kwh / 30.416666667
+        self.kwp = (quilowatt_dia / self.indice_solar) / self.eficiencia_placas
+        return self.kwp
+
+    def qtd_placas(self):
+        self.quantidade_placas = math.ceil((self.kwp / self.potencia_placas) * 1000)
+        if self.quantidade_placas % 2 != 0:
+            self.quantidade_placas += 1
+        return self.quantidade_placas
+
+    def kwp_final(self):
+        self.kwp_entregue = (self.quantidade_placas * self.potencia_placas) / 1000
+        return self.kwp_entregue
+
+class Kwh(CalculoUsinaSolar):
+    def __init__(self, kwh, potencia_placas, indice_solar, eficiencia_placas):
+        super().__init__(potencia_placas)
+        self._kwh = kwh
+        self._indice_solar = indice_solar
+        self._eficiencia_placas = eficiencia_placas
+
+    @property
+    def kwh(self):
+        return self._kwh
+
+    @property
+    def indice_solar(self):
+        return self._indice_solar
+
+    @property
+    def eficiencia_placas(self):
+        return self._eficiencia_placas
+
+
+class Kwp(CalculoUsinaSolar):
+    def __init__(self, kwp, potencia_placas):
+        super().__init__(potencia_placas)
+        self._kwp = kwp
+
+    @property
+    def kwp(self):
+        return self._kwp
---
+++ teste.py
@@ -0,0 +1,30 @@
+#Para testar importe 
+from calculo_solar import Kwh, Kwp
+
+
+kwh = Kwh(700, 440, 5.02, 0.95) #crie um objeto passando o "Valor Kwh desejado", "Potencia das Placas", "indice solarimetrico", 
+                                #"eficiencia das placas" nesse untimo em % ou seja 0.95 = 95% 
+kwh.tranforma_em_kwp() #Metodo que transforma Kwh em Kwp
+kwh.qtd_placas() #Chama o Metodo que calcula qtd de placas
+kwh.kwp_final() #Chama o Metodo que apresenta quanto em Kwp será entregue 
+#kwh.imprime_kwh() -> #Se quiser usar para teste primeiro copie e cole o metodo imprime abaixo para a classe pai.
+
+
+kwp = Kwp(6.00, 440) #crie um objeto passando o "Valor em Kwp desejado float" e "Potencia das placas"
+kwp.qtd_placas() #Chama o Metodo que calcula qtd de placas
+kwp.kwp_final() #Chama o Metodo que apresenta quanto em Kwp será entregue
+#kwp.imprime_kwp() -> #Se quiser usar para teste primeiro copie e cole o metodo imprime abaixo para a classe pai.
+
+
+
+
+    """def imprime_kwh(self):
+        print(f"Kwh é {self.kwh}")
+        print(f'KWp é:{self.kwp}')
+        print(f'Qtd é: {self.quantidade_placas} placas')
+        print(f'KWp Final é: {self.kwp_entregue}\n')
+
+    def imprime_kwp(self):
+        print(f'KWp é: {self.kwp}')
+        print(f'Qtd é: {self.quantidade_placas} placas')
+        print(f'KWp Final é: {self.kwp_entregue}')"""
